786139f81c59ae5db162fd7337adee74398bc09d
Hemant khankari <khankarih@gmail.com>
2023-02-01 22:42:50 +0530

    Hello

---
+++ GitDemo1/Sum1.java
@@ -0,0 +1,11 @@
+public class Sum1
+{
+    public static void main(String args[])
+    {
+        int a=10;
+        int b=20;
+        int sum=a+b;
+        System.out.println("Sum is: "+sum);
+         System.out.println(sum);
+    }
+}
